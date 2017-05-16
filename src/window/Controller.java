66274f515dab75d9af10ab98154b3a83eca93d54
rkarabanov <5600t0@gmail.com>
2017-05-16 22:07:11 +0300

    Add all items + link logic

--- src/window/Controller.java
+++ src/window/Controller.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Polygon;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
@@ -18,12 +19,101 @@ import java.io.IOException;
 public class Controller {
 
     @FXML
-    private Button exitBtn;
+    private Button exitBtn, shifrBtn, deshifrBtn, back, tFile,fFile,startBnt;
+
+    @FXML
+    private Label tLabel,fLabel;
+
+    @FXML
+    private TextArea tTxt,fTxt;
+
+    @FXML
+    private AnchorPane start, versions, formula;
+
+
+    public void goD(ActionEvent event) {
+        shifrBtn.setVisible(false);
+        deshifrBtn.setDisable(true);
+        exitBtn.setVisible(false);
+        versions.setVisible(true);
+    }
+    public void goS(ActionEvent event) {
+        shifrBtn.setDisable(true);exitBtn.setVisible(false);
+
+        deshifrBtn.setVisible(false);
+        versions.setVisible(true);
+
+    }
+
+    public void backAction(ActionEvent event) {
+        deshifrBtn.setVisible(true);
+        shifrBtn.setVisible(true);
+        deshifrBtn.setDisable(false);
+        shifrBtn.setDisable(false);
+        versions.setVisible(false);
+        exitBtn.setVisible(true);
+    }
+
+    public void filefile(){
+        tFileSetVisible(true);
+        fFileSetVisible(true);
+        formulaSetVisible(true);
+        versions.setVisible(false);
+    }
+
+    public void txtfile(){
+        tFileSetVisible(true);
+        fTxt.setVisible(true);
+        formulaSetVisible(true);
+        versions.setVisible(false);
+    }
+
+    public void txttxt(){
+        tTxt.setVisible(true);
+        fTxt.setVisible(true);
+        formulaSetVisible(true);
+        versions.setVisible(false);
+    }
+
+    public void filetxt(){
+        fFileSetVisible(true);
+        tTxt.setVisible(true);
+        formulaSetVisible(true);
+        versions.setVisible(false);
+    }
+
+
+    private void formulaSetVisible(boolean flag){
+        formula.setVisible(flag);
+    }
+
+    private void tFileSetVisible(boolean flag){
+        tFile.setVisible(flag);
+        tLabel.setVisible(flag);
+    }
+
+    private void fFileSetVisible(boolean flag){
+        fFile.setVisible(flag);
+        fLabel.setVisible(flag);
+    }
+
+    public void goWork(ActionEvent event){
+
+    }
+
+    public void backVariants(){
+        versions.setVisible(true);
+        formulaSetVisible(false);
+        tFileSetVisible(false);
+        fFileSetVisible(false);
+        tTxt.setVisible(false);
+        fTxt.setVisible(false);
+        tTxt.setText("");
+        fTxt.setText("");
+    }
 
 
        public void close(ActionEvent event) {
-
-
         Stage stage = (Stage) exitBtn.getScene().getWindow();
         stage.close();
     }
